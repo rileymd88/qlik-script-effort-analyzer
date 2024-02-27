a3965833f16dbec775f56a8fdeba21f73fb267c3
Renato Vieira <rvr@qlik.com>
2024-02-27 10:59:59 +0000

    Small console log feature added. Add boolean to the LOGGING variable on your env file.

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Validate required environment variables
-const requiredEnv = ['PORT', 'QLIK_HOST', 'QLIK_API_KEY', 'OPENAI_API_KEY', 'OPENAI_MODEL'];
+const requiredEnv = ['PORT', 'QLIK_HOST', 'QLIK_API_KEY', 'OPENAI_API_KEY', 'OPENAI_MODEL','LOGGING'];
 requiredEnv.forEach(env => {
     if (!process.env[env]) {
         console.error(`Error: Missing required environment variable ${env}`);
@@ -20,6 +20,8 @@ const port = process.env.PORT;
 const schema = JSON.parse(fs.readFileSync('schema.json', 'utf8'));
 
 app.use(express.json());
+
+const logging = process.env.LOGGING == 'true';
 
 // Setup Qlik Connection
 const hostConfig = {
@@ -36,10 +38,15 @@ const openai = new OpenAI({
 
 async function getScript(appId) {
     try {
+        if(logging) {console.log(`${new Date().toISOString()} Fetching QS App Script`)};
+        
         const appSession = openAppSession({ appId });
         const app = await appSession.getDoc();
         const script = await app.getScript();
         await appSession.close();
+        
+        if(logging) {console.log(`${new Date().toISOString()} Done`)};
+
         return script;
     } catch (error) {
         console.error('Error fetching script:', error);
@@ -47,8 +54,10 @@ async function getScript(appId) {
     }
 }
 
-async function getScriptDetails(script) {
+async function getScriptDetails(script, appId) {
     try {
+        if(logging) {console.log(`${new Date().toISOString()} Fetching QS App Script Details`)};
+
         const body = {
             model: process.env.OPENAI_MODEL,
             max_tokens: 4096,
@@ -65,6 +74,11 @@ async function getScriptDetails(script) {
             response_format: { "type": "json_object" },
             temperature: 0,
         };
+
+        if(logging) {console.log(`${new Date().toISOString()} Done`)};
+        if(logging) {console.log(`${new Date().toISOString()} Sending request to OpenAI`)};
+        if(logging) fs.writeFileSync("./sent-requests/" + new Date().toISOString().split('.')[0].replace(':','-').replace(':','-') + " " + appId + ".json",JSON.stringify(body, null, 2),'utf8');
+
         return await openai.chat.completions.create(body);
     } catch (error) {
         console.error('Error getting script details:', error);
@@ -79,12 +93,18 @@ app.get('/health', (req, res) => {
 app.get('/getScriptDetails/:appId', async (req, res) => {
     try {
         const { appId } = req.params;
+        if(logging) {console.log(`${new Date().toISOString()} Received new request for QS App {${appId}}`)};
+
         const script = await getScript(appId);
-        const response = await getScriptDetails(script);
+        const response = await getScriptDetails(script, appId);
         const choice = response.choices[0];
+
         if (choice.finish_reason === "length") {
             return res.json({ error: "The response from OpenAI is too long!" }).status(500);
         }
+
+        if(logging) {console.log(`${new Date().toISOString()} Received reply from OpenAI`)};
+
         res.json(JSON.parse(choice.message.content));
     } catch (error) {
         res.json({ error: error.message || "An error occurred." }).status(500);
